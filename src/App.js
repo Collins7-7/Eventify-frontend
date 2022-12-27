74c262de153635b7eaefabc51ed76607073db8ae
tania <taniaosano15@gmail.com>
2022-12-27 18:16:09 +0300

    created react components (home, categories, signup and login) for the navbar section. Worked with React-Router-Dom for a responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Category from './components/Category'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+  <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path= '/' element={<Home/>} exact/>
+        <Route path= '/categories' element={<Category />} exact />
+        <Route path= '/signup' element={< Signup />} exact />
+        <Route path= '/login' element={<Login />} exact />
+      </Routes>
+    </Router>
+  </>
+      
+    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 function Navbar() {
+
+
     return(
         <>
         <nav>
             <h1>Eventify</h1>
-
         <div>
-            <ul id="navbar">
-                <li><a href ="index.html">Home</a></li>
-                <li><a href ="index.html">Category</a></li>
-                <li><a href ="index.html">Signup</a></li>
-                <li><a href ="index.html">Login</a></li>
-
+            <ul className="navbar">
+                <Link to='/'><li>HOME</li></Link>
+                <Link to='/categories'><li>CATEGORIES</li></Link>
+                <Link to='/signup'><li>SIGNUP</li></Link>
+                <Link to='/login'><li>LOGIN</li></Link>
             </ul>
         </div>
         </nav>
