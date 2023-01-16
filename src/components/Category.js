30a4dcb3928f7c24612d95d89a22e6ae0d5aa76e
stanford kangethe <stanfordkangethe16@gmail.com>
2023-01-16 06:44:51 +0300

    fetched from the server successfully and rendered the cards.

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+
 
 function Category(){
+  const [categories, setCategories] = useState([])
 
-    return(
-        <h1>view your event categories</h1>
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/categories")
+    .then((r) => r.json())
+    .then((categories) => setCategories(categories))
+  }, [])
+
+  return(
+    <section className='section'>
+      {categories.map((category) => {
+        return (
+          <div className='card-image' key={category.id}>
+            <img src={category.image_url} alt='category-img' className='image'/>
+            <div className='overlay'>
+              <h3>{category.name}</h3>
+              <div>
+                <h4>{category.description}</h4>
+                <button className='btn'>View Events</button>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </section>
     )
 }
 
