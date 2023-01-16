0a970c91df528a3d3f6447c9f9a8bbfcfc84eff1
moringa <oscarokumu47@gmail.com>
2023-01-16 20:55:25 +0300

    Removed an used component bookedForm

--- src/components/BookedForm.js
+++
@@ -1,9 +0,0 @@
-const BookedForm = () => {
-    return (
-      <div>
-        <h2>Booking Confirmed!</h2>
-        <p>Thank you for booking. We will contact you soon.</p>
-      </div>
-    )
-  }
-export default BookedForm  ;
