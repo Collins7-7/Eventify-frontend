fe23779462413e6e4286be43a754b42fd0028f7c
moringa <oscarokumu47@gmail.com>
2023-01-16 10:08:25 +0300

    styled booking form

---
+++ src/components/BookedForm.js
@@ -0,0 +1,9 @@
+const BookedForm = () => {
+    return (
+      <div>
+        <h2>Booking Confirmed!</h2>
+        <p>Thank you for booking. We will contact you soon.</p>
+      </div>
+    )
+  }
+export default BookedForm  ;
