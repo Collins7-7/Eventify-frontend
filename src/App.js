aabbe947923ba9ed5b4fa5670f30552e06af0873
moringa <oscarokumu47@gmail.com>
2023-01-16 10:31:44 +0300

    styled booking form && added a form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -6,6 +7,8 @@ import Contact from './components/Contact';
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Home from './components/Home'
+import EventBookingForm from './components/BookingForm/BookingForm';
+// import BookingForm from './components/BookingForm/BookingForm'
 
 function App() {
   return (
@@ -18,7 +21,9 @@ function App() {
         <Route path= '/contact' element={<Contact />} exact />
         <Route path= '/signup' element={< Signup />} exact />
         <Route path= '/login' element={<Login />} exact />
+        {/* <Route path='/booking' element={<BookingForm/>} exact /> */}
       </Routes>
+      <EventBookingForm/>
     </Router>
   </>
       
